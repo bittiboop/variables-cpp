554e7ad73ac7ab275a02dec6b8d198d558d93669
bittiboop <sstrelkova15gmail.com>
2024-04-17 19:54:15 +0300

    made the second ex :)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 
 int main() {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, 14);
+	cout << "\"U lukomor`a dub zelenij,\nZlataya zep na dupe tom,\nI dnem i nochju kot uchenij\nvse hodit po cepi krugom\"" << endl;
+	SetConsoleTextAttribute(hConsole, 7);
+	system("pause");
 	return 0;
 }
