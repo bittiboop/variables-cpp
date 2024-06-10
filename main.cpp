a58b3b4eb9451150cd3b07469871ea40b8fa3b9c
bittiboop <sstrelkova15@gmail.com>
2024-06-10 19:54:46 +0300

    made ex1

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 
 int main() {
-	cout << "Home Work :)" << endl;
-	cout << "Home Work :)" << endl;
-	cout << "Home Work :)" << endl;
+	int r0 = 0;
+	int r1, r2, r3;
+	cin >> r1 >> r2 >> r3;
+	r0 == 1 / r1 + 1 / r2 + 1 / r3;
+	cout << 1 / r0 << endl;
 	return 0;
 }
